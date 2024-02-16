88b94d2a83ffc9dbf4703f265ca9778bd2b4c993
Jihunkim95 <darak551@naver.com>
2024-02-16 16:23:43 +0900

    [Feat ]: SwiftUI 생성 및 FireStore관련 라이브러리 설치

---
+++ PushDemo/PushDemo/PushDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushDemoApp.swift
+//  PushDemo
+//
+//  Created by 김지훈 on 2024/02/16.
+//
+
+import SwiftUI
+
+@main
+struct PushDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
