377f15d4cc4c90841d496ebaca9c1fa4fa9e94ef
Jihunkim95 <darak551@naver.com>
2024-02-16 16:23:43 +0900

    [Feat ]: SwiftUI 생성 및 Push알림 관련 라이브러리 설치

---
+++ PushDemo/PushDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushDemo
+//
+//  Created by 김지훈 on 2024/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
